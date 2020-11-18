167737f4fe2d52838979197814e92d951077d56d
root <ayoub.errkhis@usms.ac.ma>
2020-11-18 02:42:55 +0100

    first

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
+import {File} from '@ionic-native/file/ngx'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  photos:any = [];
+  constructor(public camera : Camera ,public file : File) {}
+  upload(){
+    var options:CameraOptions={
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData)=>{
+      let filename = imageData.substring(imageData.lastIndexOf('/')+1);
+      let path = imageData.substring(0,imageData.lastIndexOf('/')+1);
+      this.file.readAsDataURL(path,filename).then((base64data)=>{
+        this.photos.push(base64data);
+      })
+    })
+  }
 }
